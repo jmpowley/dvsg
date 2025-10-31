fa0c3ea29c6c759977e092455ecbe8c2f2c18457
jmpowley <jonahpowley1@gmail.com>
2025-10-31 23:22:24 +0000

    Changed src

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="dvsg",
     version="0.1.0",
-    description="Tools used for the calcualtion of DVSG values",
-    author="Jonah Powley",
-    packages=find_packages(),
+    description="Tools used for the calculation of DVSG values",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    include_package_data=True,
     install_requires=[
-        "astropy"
+        
     ],
-    python_requires=">=3.6",
 )
