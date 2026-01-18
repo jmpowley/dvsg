b3521ebcccb43d39aed2ff8db29ea1d0bd02ed5a
jmpowley <jonahpowley1@gmail.com>
2026-01-18 00:25:11 +0000

    Add conditional SNR mask

--- src/dvsg/plotting.py
+++ src/dvsg/plotting.py
@@ -141,11 +141,11 @@ def create_stellar_gas_residual_maps_for_plotting(plateifu : str, **dvsg_kwargs)
     bin_snr_flat = mask_binned_map(bin_snr, sv_mask, bin_ids)  # use stellar velocity mask
 
     # Apply SNR threshold
-    # sv_flat, gv_flat = apply_snr_threshold(sv_flat, gv_flat, sv_ivar_flat, gv_ivar_flat, **dvsg_kwargs)
-    sv_flat, gv_flat = apply_bin_snr_threshold(sv_flat, gv_flat, bin_snr_flat, **dvsg_kwargs)
+    if dvsg_kwargs.get("snr_threshold") is not None:
+        sv_flat, gv_flat = apply_bin_snr_threshold(sv_flat, gv_flat, bin_snr_flat, **dvsg_kwargs)
 
     # Sigma clip and normalise maps
-    sv_excl, gv_excl = apply_sigma_clip(sv_flat, gv_flat)
+    sv_excl, gv_excl = apply_sigma_clip(sv_flat, gv_flat, n=3)
     sv_norm, gv_norm = normalise_map(sv_excl, gv_excl, **dvsg_kwargs)
 
     # Calculate residual map
